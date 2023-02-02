544214259c1a1888eb62db6a08e7dfa61a1c89dc
jahanbaev <sjahanbaev@gmail.com>
2023-02-02 13:10:09 +0500

    feat: added builder page

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@ import { RouterLink, RouterView } from "vue-router";
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/builder">Builder</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/pages/BuilderPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="">
+        <LeftBar/>
+    </div>
+</template>
+
+<script lang="ts">
+import LeftBar from './../components/Builder/LeftBar.vue'
+ export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        LeftBar
+    }
+ }
+</script>
