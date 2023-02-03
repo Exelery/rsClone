de549eca4e068adcc09909a9a841de7d6fa9a660
Anatoliy Komarov <admin@komaroff.biz>
2023-02-03 12:14:39 +0300

    feat: create header && footer components

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,21 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/builder">Builder</RouterLink>
-      </nav>
-    </div>
-  </header>
+<!--  <header>-->
+<!--    <div class="wrapper">-->
+<!--      <nav>-->
+<!--        <RouterLink to="/">Home</RouterLink>-->
+<!--        <RouterLink to="/about">About</RouterLink>-->
+<!--        <RouterLink to="/builder">Builder</RouterLink>-->
+<!--      </nav>-->
+<!--    </div>-->
+<!--  </header>-->
+
 
   <RouterView />
 </template>
 
 <style>
-@import "@/assets/base.css";
-
 #app {
   margin: 0;
 }
---
+++ src/components/HeaderBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <header id="header" class="fixed-top header-inner-pages">
+    <div class="container d-flex align-items-center">
+      <h1 class="logo me-auto"><a href="index.html">Page builder</a></h1>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><RouterLink to="/" class="nav-link scrollto active">Home</RouterLink></li>
+          <li><RouterLink to="/about" class="nav-link scrollto ">About</RouterLink></li>
+          <li><RouterLink to="/builder" class="nav-link scrollto ">Builder</RouterLink></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderBox"
+}
+</script>
+
+<style scoped>
+
+</style>
